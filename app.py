cd1bdc98a096530ed89a3ee70d72c6d8ed78d8dd
Bruno Gomes <brunoshy@gmail.com>
2023-04-04 09:48:13 -0300

    main: Fix api

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def chat():
     return jsonify({'message': message})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=True)
+    app.run(host='0.0.0.0', port='80', debug=True)
 
 
 # sudo nohup python3 -u /home/monga/monga-applications-lab/app.py
