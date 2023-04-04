6d6a2793ad532a01ce90f41b48e7a67b200a3d04
Bruno Gomes <brunoshy@gmail.com>
2023-04-04 01:05:05 -0300

    main: pipeline job clone-repo linux

---
+++ scripts/clone-repo.sh
@@ -0,0 +1,4 @@
+sudo kill -9 `sudo lsof -t -i:80`
+rm -rf monga-applications-lab
+git clone https://github.com/BrunoPolezaGomes/monga-applications-lab.git
+sudo nohup python3 -u /home/monga/monga-applications-lab/app.py
