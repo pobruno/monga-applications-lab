5601330a36534dfeb9536cf51ea05199af0b3b69
Bruno Gomes <brunoshy@gmail.com>
2023-04-04 01:56:08 -0300

    main: Create API ChatGPT

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+import openai
+
 
 app = Flask(__name__)
-
+# config.py config.DevelopmentConfig.OPENAI_KEY
+openai.api_key = config.DevelopmentConfig.OPENAI_KEY
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World! V1.0.2'
 
-@app.route('/api', methods=['POST', 'GET'])
+@app.route('/api/chat', methods=['POST'])
 def chat():
-    response = {
-        'message': 'Hello, ChatGPT!'
-    }
-    if request.method == 'POST':
-        data = request.get_json()
-        # código para processar o JSON aqui
-        response['message'] = chatgpt_response
-    return jsonify(response)
+    # Obtém a mensagem do cliente do corpo da solicitação
+    data = request.json
+    prompt = data['prompt']
 
+    # Chama o modelo ChatGPT da OpenAI para gerar uma resposta
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+
+    # Obtém a resposta gerada pelo modelo e retorna ao cliente
+    message = response.choices[0].text.strip()
+    return jsonify({'message': message})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='80', debug=True)
+
+# sudo nohup python3 -u /home/monga/monga-applications-lab/app.py
--- scripts/clone-repo.sh
+++ scripts/clone-repo.sh
@@ -3,8 +3,3 @@ rm -rf monga-applications-lab
 git clone https://github.com/BrunoPolezaGomes/monga-applications-lab.git
 sudo cp /home/monga/config.py /home/monga/monga-applications-lab/
 sudo nohup python3 -u /home/monga/monga-applications-lab/app.py
-
-
-
-
-
