cf806147ad964a99c481422a014c5536030dbb0b
Bruno Gomes <brunoshy@gmail.com>
2023-04-04 01:31:50 -0300

    main: Create config.py

--- scripts/clone-repo.sh
+++ scripts/clone-repo.sh
@@ -1,4 +1,10 @@
 sudo kill -9 `sudo lsof -t -i:80`
 rm -rf monga-applications-lab
 git clone https://github.com/BrunoPolezaGomes/monga-applications-lab.git
+sudo cp /home/monga/config.py /home/monga/monga-applications-lab/
 sudo nohup python3 -u /home/monga/monga-applications-lab/app.py
+
+
+
+
+
