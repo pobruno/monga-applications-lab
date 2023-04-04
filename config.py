d84a697585949fc890484a376458a71fc95ad969
Bruno Gomes <brunoshy@gmail.com>
2023-04-04 20:35:07 -0300

    main: Create bot.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config(object):
 class DevelopmentConfig(Config):
     SECRET_KEY = "this-is-a-super-secret-key"
     OPENAI_KEY = 'this-is-a-openai-key'
+    MONGAI_BOT_TOKEN = 'this-is-a-mongai-bot-token'
 
 config = {
     'development': DevelopmentConfig,
--- scripts/clone-repo.sh
+++ scripts/clone-repo.sh
@@ -3,4 +3,5 @@ rm -rf monga-applications-lab
 git clone https://github.com/BrunoPolezaGomes/monga-applications-lab.git
 sudo cp /home/monga/config.py /home/monga/monga-applications-lab/
 sudo nohup python3 -u /home/monga/monga-applications-lab/app.py
+#sudo nohup python3 -u /home/monga/monga-applications-lab/bot.py
 sudo nohup python3 -u /home/monga/monga-applications-lab/app.py
