365990141e3acdb63e701d59efe739d775d42c7a
Bruno Gomes <brunoshy@gmail.com>
2023-04-05 00:21:35 -0300

    front: Create clone front

--- scripts/clone-repo.sh
+++ scripts/clone-repo.sh
@@ -1,7 +1,12 @@
 sudo kill -9 `sudo lsof -t -i:80`
+sudo kill -9 `sudo lsof -t -i:3000`
 rm -rf monga-applications-lab
+rm -rf mgpt
 git clone https://github.com/BrunoPolezaGomes/monga-applications-lab.git
+git clone https://github.com/robjunior/mgpt.git
 sudo cp /home/monga/config.py /home/monga/monga-applications-lab/
 sudo nohup python3 -u /home/monga/monga-applications-lab/app.py
 #sudo nohup python3 -u /home/monga/monga-applications-lab/bot.py
 sudo nohup python3 -u /home/monga/monga-applications-lab/app.py
+
+#sudo python3 /home/monga/monga-applications-lab/bot.py
