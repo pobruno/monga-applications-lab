599fd3dd5b846aca6b1fd2cacfbc665d4bc69308
Bruno Gomes <brunoshy@gmail.com>
2023-04-08 01:13:27 -0300

    main: init new linux env

---
+++ README.md
@@ -0,0 +1,27 @@
+##Workflow preparação de ambiente linux
+
+
+#Packages Necessários
+- python3
+- python3-pip
+- python-virtualenv
+´´´
+sudo apt update && sudo apt upgrade
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+sudo apt install dotenv -y
+
+#sudo apt install python-virtualenv -y
+´´´
+
+#Pip Dependences
+
+´´´
+sudo pip3 install virtualenv
+sudo pip3 install dotenv
+sudo pip3 install flask
+sudo pip3 install flask_cors
+sudo pip3 install openai
+sudo pip3 install python-dotenv
+sudo pip3 install requests
+´´´
--- app.py
+++ app.py
@@ -1,68 +1,72 @@
+from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import config
 import requests
 import openai
+import os
 
-# config.py config.DevelopmentConfig.OPENAI_KEY
-openai.api_key = config.DevelopmentConfig.OPENAI_KEY
+load_dotenv()
+
+openai.api_key = os.getenv('OPENAI_KEY')
 
 app = Flask(__name__)
 CORS(app)
 
-# Define the API endpoint
-@app.route('/api/v1/message', methods=['POST'])
+## Define the API endpoint
+# Version v2
+@app.route('/api/v2/message', methods=['POST'])
 def generate_response():
-    # Parse the request data
     data = request.get_json()
-
-    # Generate the response using the OpenAI API
     prompt = f"Conversation:\nUser: {data['text']}\nAI:"
     response = openai.Completion.create(
-        engine='text-davinci-002',
+        engine='text-davinci-003',
         prompt=prompt,
         max_tokens=1024,
         n=1,
         stop=None,
         temperature=0.7
     )
-
-    # Extract the response from the OpenAI API and return it as JSON
     message = response.choices[0].text.strip()
     return jsonify({'message': message})
+
+# Version v3
+@app.route('/api/v3/message', methods=['POST'])
+def message():
+   data = request.get_json()
+   prompt = data['prompt']
+   response = openai.ChatCompletion.create(
+       model="gpt-3.5-turbo",
+       messages=[
+           {"role": "system", "content": "You are a helpful assistant."},
+           {"role": "user", "content": prompt}
+       ]
+   )
+   message = response.choices[0].text.strip()
+   return jsonify({'message': message})
+
+# Versoin v4
+@app.route('/api/v4/message', methods=['POST'])
+def message():
+   data = request.get_json()
+   prompt = data['prompt']
+   response = openai.ChatCompletion.create(
+       model="text-davinci-002",
+       messages=[
+           {"role": "system", "content": "You are a helpful assistant."},
+           {"role": "user", "content": prompt}
+       ]
+   )
+   message = response.choices[0].message.content.strip()
+   return jsonify({'message': message})
+
+# Version v5
+@app.route('/')
+def hello_world():
+    return 'Hello World! V1.0.2'
+
 
 # Start the Flask app
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='80', debug=True)
 
-
-# sudo nohup python3 -u /home/monga/monga-applications-lab/app.py
-#response = openai.ChatCompletion.create(
-#    model="text-davinci-002",
-#    messages=[
-#        {"role": "system", "content": "You are a helpful assistant."},
-#        {"role": "user", "content": prompt}
-#    ]
-#)
-#message = response.choices[0].message.content.strip()
-
-# @app.route('/')
-# def hello_world():
-#    return 'Hello World! V1.0.2'
-
-# @app.route('/api/v1/message', methods=['POST'])
-# def message():
-#    # Obtém a mensagem do cliente do corpo da solicitação
-#    data = request.json
-#    prompt = data['prompt']
-#    # Chama o modelo ChatGPT da OpenAI para gerar uma resposta
-#    response = openai.ChatCompletion.create(
-#        model="gpt-3.5-turbo",
-#        messages=[
-#            {"role": "system", "content": "You are a helpful assistant."},
-#            {"role": "user", "content": prompt}
-#        ]
-#    )
-#    # Obtém a resposta gerada pelo modelo e retorna ao cliente
-#    message = response.choices[0].text.strip()
-#    return jsonify({'message': message})
